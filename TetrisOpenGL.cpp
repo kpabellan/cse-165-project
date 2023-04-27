0afdc369c30801c72b9d4aa17c45bc8af6c53a20
Manuel Alejandro Macias <79296553+MMacias7@users.noreply.github.com>
2023-04-27 01:56:12 -0700

    Add files via upload
    This is the 2nd update to the OpenGL File. I have now added the tetris shapes.

--- TetrisOpenGL.cpp
+++ TetrisOpenGL.cpp
@@ -4,6 +4,71 @@
 const int WINDOW_WIDTH = 300;
 const int WINDOW_HEIGHT = 600;
 
+void drawSquare(float x, float y, float size) {
+   glBegin(GL_QUADS); //This creates quadrilaterals -- We will be using this to create the shapes of the tetris figures.
+   glVertex2f(x, y); //The following 4 lines are creating the 4 different sides of a quadrilateral (a square of 1 pixel*).
+   glVertex2f(x + size, y);
+   glVertex2f(x + size, y + size);
+   glVertex2f(x, y + size);
+   glEnd();
+}
+
+//------------------------------------------------------------
+
+//Shapes for the tetris figures
+void L_shape(float x, float y, float size) {
+   glColor3f(0.0f, 0.0f, 1.0f);
+   drawSquare(x, y, size);
+   drawSquare(x, y + size, size);  
+   drawSquare(x, y + 2*size, size);
+   drawSquare(x + size, y, size);
+}
+void I_shape(float x, float y, float size) {
+    glColor3f(1.0f, 0.0f, 1.0f);
+    drawSquare(x, y, size);
+    drawSquare(x, y + size, size);  
+    drawSquare(x, y + 2*size, size);
+    drawSquare(x, y + 3*size, size);
+}
+void O_shape(float x, float y, float size) {
+    glColor3f(0.0f, 1.0f, 1.0f);
+    drawSquare(x, y, size);
+    drawSquare(x, y + size, size);  
+    drawSquare(x + size, y, size);
+    drawSquare(x + size, y + size, size);
+}
+void T_shape(float x, float y, float size) {
+    glColor3f(1.0f, 0.0f, 0.0f);
+    drawSquare(x, y, size);
+    drawSquare(x + size, y, size);  
+    drawSquare(x - size, y, size);
+    drawSquare(x, y + size, size);
+}
+void S_shape(float x, float y, float size) {
+    glColor3f(0.0f, 1.0f, 0.0f);
+    drawSquare(x, y, size);
+    drawSquare(x - size, y, size);  
+    drawSquare(x, y + size, size);
+    drawSquare(x + size, y + size, size);
+}
+void Z_shape(float x, float y, float size) {
+    glColor3f(1.0f, 1.0f, 0.0f);
+    drawSquare(x, y, size);
+    drawSquare(x + size, y, size);  
+    drawSquare(x, y + size, size);
+    drawSquare(x - size, y + size, size);
+}
+void J_shape(float x, float y, float size) {
+    glColor3f(1.0f, 0.5f, 0.0f);
+    drawSquare(x, y, size);
+    drawSquare(x, y + size, size);  
+    drawSquare(x + size, y, size);
+    drawSquare(x + 2 * size, y, size);
+}
+
+//--------------------------------------------------------
+
+
 void init() {
    glClearColor(0.0, 0.0, 0.0, 0.0); //Black Background for the square of the Tetris Game
    glMatrixMode(GL_PROJECTION);
@@ -11,6 +76,8 @@ void init() {
    glOrtho(0, 10, 0, 20, -1, 1); //Specifies the size of the projection on the computer screen. 
    glMatrixMode(GL_MODELVIEW);
 }
+
+//----------------------------------------------------------
 
 void display() {
    glColor3f(1.0, 1.0, 1.0); // white color for the grid lines
@@ -30,10 +97,19 @@ void display() {
       glVertex2f(10, i);
    }
    glEnd();
-
+/*
+   L_shape(2.0, 2.0, 1.0);
+   I_shape(5.0,5.0, 1.0);
+   O_shape(0.0, 0.0, 1.0);
+   T_shape(7.0, 0.0, 1.0);
+   S_shape(2.0, 5.0, 1.0);
+   Z_shape(2.0, 10.0, 1.0);
+   J_shape(6.0, 11.0, 1.0);
+*/
    glutSwapBuffers(); //There's 2 buffers, and we are always switching them to make it work faster.
 }
 
+//---------------------------------------------------------------
 
 int main(int argc, char** argv) {
 
