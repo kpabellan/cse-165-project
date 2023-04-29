c2d24a552bbfd8ed0619a3bc127b5b17d310d68f
Kobey Pabellan <pabellank@gmail.com>
2023-04-29 06:36:01 -0700

    Progress on game logic

--- tetris.cpp
+++ tetris.cpp
@@ -1,4 +1,5 @@
 #include <GL/glut.h>
+#include <iostream>
 #include <chrono>
 #include <thread>
 #include <cstdlib>
@@ -7,9 +8,10 @@
 
 float piece_x = 1.0f;
 float piece_y = 1.0f;
-int blockType = 0;
-Tetris::Shape shape = Tetris::Shape::I;
+int blockType = 1; // 1 = I, 2 = J, 3 = L, 4 = O, 5 = S, 6 = T, 7 = Z
 int pieceOrientation = 0; // 0 = original orientation, 1 = 90 degree rotation, 2 = 180 degree rotation, 3 = 270 degree rotation
+int gameBoard[20][10]; // [row][column]
+Tetris::Shape shape = Tetris::Shape::I;
 
 using namespace std::this_thread; // Sleep_for, sleep_until
 using namespace std::chrono; // Nanoseconds, system_clock, seconds
@@ -19,29 +21,6 @@ const int WINDOW_WIDTH = 600;
 const int WINDOW_HEIGHT = 1200;
 const int SPAWN_INTERVAL_MS = 500;
 
-void drawSquare(float x, float y, float size) {
-  glBegin(GL_QUADS); // This creates quadrilaterals -- We will be using this to create the shapes of the tetris figures.
-  glVertex2f(x, y); // The following 4 lines are creating the 4 different sides of a quadrilateral (a square of 1 pixel*).
-  glVertex2f(x + size, y);
-  glVertex2f(x + size, y + size);
-  glVertex2f(x, y + size);
-  glEnd();
-}
-
-void drawTetromino(float x, float y, float size, Tetris::Shape shape, int orientation) {
-  Tetris tetris(shape);
-  glColor3f(tetris.color[0], tetris.color[1], tetris.color[2]);
-  const auto & tetromino = tetris.tetrominoes[orientation];
-
-  for (int row = 0; row < tetromino.size(); ++row) {
-    for (int col = 0; col < tetromino[row].size(); ++col) {
-      if (tetromino[row][col]) {
-        drawSquare(x + col * size, y + row * size, size);
-      }
-    }
-  }
-}
-
 void init() {
   glClearColor(0.0, 0.0, 0.0, 0.0); // This clears all filters/colors and sets the background to black
   glMatrixMode(GL_PROJECTION);
@@ -50,34 +29,71 @@ void init() {
   glMatrixMode(GL_MODELVIEW);
 }
 
-void spawnBlock() {
+// Draws a square at the specified location with the specified size
+void drawBlock(float x, float y, float size) {
+  // Draw the square
+  glBegin(GL_QUADS);
+  glVertex2f(x, y);
+  glVertex2f(x + size, y);
+  glVertex2f(x + size, y + size);
+  glVertex2f(x, y + size);
+  glEnd();
+
+  // Draw the gridlines
+  glColor3f(1.0, 1.0, 1.0); // Set the color to white for the gridlines
+  glBegin(GL_LINE_LOOP);
+  glVertex2f(x, y);
+  glVertex2f(x + size, y);
+  glVertex2f(x + size, y + size);
+  glVertex2f(x, y + size);
+  glEnd();
+}
+
+// Draws a tetromino at the specified location with the specified size
+void drawTetromino(float x, float y, float size, Tetris::Shape shape, int orientation) {
+  Tetris tetris(shape);
+  const auto & tetromino = tetris.tetrominoes[orientation];
+
+  for (int row = 0; row < tetromino.size(); row++) {
+    for (int col = 0; col < tetromino[row].size(); col++) {
+      if (tetromino[row][col]) {
+        glColor3f(tetris.color[0], tetris.color[1], tetris.color[2]);
+        drawBlock(x + col * size, y + row * size, size);
+      }
+    }
+  }
+}
+
+// Spawns a new block at the top of the board
+void spawnTetromino() {
   srand(time(0));
   blockType = rand() % 7 + 1;
-  if (blockType == 0) {
+  if (blockType == 1) {
     shape = Tetris::Shape::I;
-  } else if (blockType == 1) {
-    shape = Tetris::Shape::J;
   } else if (blockType == 2) {
-    shape = Tetris::Shape::L;
+    shape = Tetris::Shape::J;
   } else if (blockType == 3) {
-    shape = Tetris::Shape::O;
+    shape = Tetris::Shape::L;
   } else if (blockType == 4) {
-    shape = Tetris::Shape::S;
+    shape = Tetris::Shape::O;
   } else if (blockType == 5) {
-    shape = Tetris::Shape::T;
+    shape = Tetris::Shape::S;
   } else if (blockType == 6) {
+    shape = Tetris::Shape::T;
+  } else if (blockType == 7) {
     shape = Tetris::Shape::Z;
   }
   piece_x = 3.0f;
   piece_y = 17.0f;
 }
 
-bool validMove(float x, float y, int pieceOrientation, int dx, int dy, Tetris::Shape shape) {
+// Checks if the block can move to the specified location
+bool validMove(float x, float y, int pieceOrientation, int newOrientation, int dx, int dy, Tetris::Shape shape) {
   Tetris tetris(shape);
-  const auto & tetromino = tetris.tetrominoes[pieceOrientation];
+  const auto & tetromino = tetris.tetrominoes[newOrientation];
 
-  for (int row = 0; row < tetromino.size(); ++row) {
-    for (int col = 0; col < tetromino[row].size(); ++col) {
+  for (int row = 0; row < tetromino.size(); row++) {
+    for (int col = 0; col < tetromino[row].size(); col++) {
       if (tetromino[row][col]) {
         float newX = x + col + dx;
         float newY = y + row + dy;
@@ -85,25 +101,108 @@ bool validMove(float x, float y, int pieceOrientation, int dx, int dy, Tetris::S
         if (newX < 0 || newX > 9 || newY < 0 || newY > 19) {
           return false; // Out of bounds
         }
-        // Check for collision code here
+
+        if (gameBoard[static_cast < int > (newY)][static_cast < int > (newX)]) {
+          return false; // Collision with other block on the board
+        }
       }
     }
   }
-
   return true;
 }
 
-void timer(int value) {
-  if (validMove(piece_x, piece_y, pieceOrientation, 0, -1, shape)) {
-    piece_y -= 1.0f;
-  }
+void clearRows() {
+  for (int row = 0; row < 20; row++) {
+    bool rowCompleted = true;
+    for (int col = 0; col < 10; col++) {
+      if (gameBoard[row][col] == 0) {
+        rowCompleted = false;
+        break;
+      }
+    }
 
-   if (piece_y <= 0.0f) {
-    spawnBlock();
+    if (rowCompleted) {
+      // Shift all the rows above the completed row down by one
+      for (int j = row; j < 19; j++) {
+        for (int col = 0; col < 10; col++) {
+          gameBoard[j][col] = gameBoard[j + 1][col];
+        }
+      }
+
+      // Clear the top row
+      for (int col = 0; col < 10; col++) {
+        gameBoard[19][col] = 0;
+      }
+
+      // Check the same row again in case multiple rows have been completed
+      --row;
+    }
+  }
+}
+
+void timer(int value) {
+  int newOrientation = pieceOrientation;
+  if (validMove(piece_x, piece_y, pieceOrientation, newOrientation, 0, -1, shape)) {
+    piece_y -= 1.0f;
+  } else {
+    Tetris tetris(shape);
+    const auto & tetromino = tetris.tetrominoes[pieceOrientation];
+
+    for (int row = 0; row < tetromino.size(); row++) {
+      for (int col = 0; col < tetromino[row].size(); col++) {
+        if (tetromino[row][col]) {
+          int boardRow = static_cast < int > (piece_y) + row;
+          int boardCol = static_cast < int > (piece_x) + col;
+          gameBoard[boardRow][boardCol] = blockType;
+        }
+      }
+    }
+
+    // Check if game is over
+    for (int col = 0; col < 10; col++) {
+      if (gameBoard[19][col] != 0) {
+        exit(0);
+      }
+    }
+
+    clearRows();
+
+    spawnTetromino();
   }
 
   glutPostRedisplay();
   glutTimerFunc(SPAWN_INTERVAL_MS, timer, 0);
+}
+
+// Draws the board so that the user can see the blocks that have already fallen
+void drawBoard(float size) {
+  for (int row = 0; row < 20; row++) {
+    for (int col = 0; col < 10; col++) {
+      Tetris tetris(Tetris::Shape::I); // Initialize the Tetris object with a default shape
+      if (gameBoard[row][col]) {
+        if (gameBoard[row][col] == 1) {
+          tetris = Tetris(Tetris::Shape::I);
+        } else if (gameBoard[row][col] == 2) {
+          tetris = Tetris(Tetris::Shape::J);
+        } else if (gameBoard[row][col] == 3) {
+          tetris = Tetris(Tetris::Shape::L);
+        } else if (gameBoard[row][col] == 4) {
+          tetris = Tetris(Tetris::Shape::O);
+        } else if (gameBoard[row][col] == 5) {
+          tetris = Tetris(Tetris::Shape::S);
+        } else if (gameBoard[row][col] == 6) {
+          tetris = Tetris(Tetris::Shape::T);
+        } else if (gameBoard[row][col] == 7) {
+          tetris = Tetris(Tetris::Shape::Z);
+        } else {
+          // No block in this cell, draw nothing
+          continue;
+        }
+        glColor3f(tetris.color[0], tetris.color[1], tetris.color[2]);
+        drawBlock(col * size, row * size, size);
+      }
+    }
+  }
 }
 
 void display() {
@@ -111,8 +210,8 @@ void display() {
   glColor3f(1.0, 1.0, 1.0); // White color for the grid lines
 
   // Draw grid lines
-  glLineWidth(1.0); // 1 pixel for the width of the grid lines
-  glBegin(GL_LINES); // primitive to create straight lines
+  glLineWidth(1.0);
+  glBegin(GL_LINES);
 
   // Vertical lines
   for (int i = 0; i <= 10; i++) {
@@ -120,41 +219,50 @@ void display() {
     glVertex2f(i, 20);
   }
 
-  //Horizontal Lines---->
+  //Horizontal lines
   for (int i = 0; i <= 20; i++) {
     glVertex2f(0, i);
     glVertex2f(10, i);
   }
   glEnd();
 
+  // Draw the board (blocks that have already fallen)
+  drawBoard(1.0f);
+
+  // Draw the current tetromino
   drawTetromino(piece_x, piece_y, 1.0f, shape, pieceOrientation);
 
   glutSwapBuffers();
 }
 
 void handleInput(int key, int x, int y) {
+  int newOrientation = pieceOrientation;
+
   switch (key) {
   case GLUT_KEY_UP:
-    pieceOrientation = (pieceOrientation + 1) % 4;
+    newOrientation = (pieceOrientation + 1) % 4;
+    if (validMove(piece_x, piece_y, pieceOrientation, newOrientation, 0, 0, shape)) {
+      pieceOrientation = newOrientation;
+    }
     glutPostRedisplay(); // Updates the displaying image when an action has occurred
     break;
 
   case GLUT_KEY_RIGHT:
-    if (validMove(piece_x, piece_y, pieceOrientation, 1, 0, shape)) {
+    if (validMove(piece_x, piece_y, pieceOrientation, newOrientation, 1, 0, shape)) {
       piece_x += 1.0f;
     }
     glutPostRedisplay();
     break;
 
   case GLUT_KEY_LEFT:
-    if (validMove(piece_x, piece_y, pieceOrientation, -1, 0, shape)) {
+    if (validMove(piece_x, piece_y, pieceOrientation, newOrientation, -1, 0, shape)) {
       piece_x -= 1.0f;
     }
     glutPostRedisplay();
     break;
 
   case GLUT_KEY_DOWN:
-    if (validMove(piece_x, piece_y, pieceOrientation, 0, -1, shape)) {
+    if (validMove(piece_x, piece_y, pieceOrientation, newOrientation, 0, -1, shape)) {
       piece_y -= 1.0f;
     }
     glutPostRedisplay();
@@ -169,7 +277,7 @@ int main(int argc, char ** argv) {
   glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
   glutCreateWindow("TETRIS");
   init();
-  spawnBlock();
+  spawnTetromino();
   glutDisplayFunc(display);
   glutSpecialFunc(handleInput);
   glutTimerFunc(1000, timer, 0);
